a62e9e728dd957a754f5b2413252a45068f65740
Christian Nancy <christian@wooden-spoon.com>
2023-05-03 20:34:27 -0700

    Mock payload

--- GetOneDriveTeamMappings/run.ps1
+++ GetOneDriveTeamMappings/run.ps1
@@ -12,14 +12,39 @@ if (-not $userEmail) {
     $userEmail = $Request.Body.userEmail
 }
 
-$body = ConvertTo-Json(@{ReturnCode = 0; Message = "This HTTP triggered function executed successfully. Pass userEmail in the query string to request the OneDrive Teams mapping information for that user."})
+$body = @{ReturnCode = 1; Message = "This HTTP triggered function executed successfully. Pass userEmail in the query string to request the OneDrive Teams mapping information for that user."}
 
 if ($userEmail) {
-    $body = ConvertTo-Json(@{ReturnCode = 0; Message = "Returning OneDrive Teams mapping information for $userEmail."})
+    $body = @{ReturnCode = 0; LibraryIDs = @(
+        @{
+            TenantTitle = 'WST'
+            WebTitle     = 'Admin'
+            ChannelTitle = 'General'
+            GroupId      = 'fe4836d1-495d-43d0-9f59-b831e928c7d2'
+            SiteId       = '4c15fc62-380f-4f05-8792-95d7e7e9c418'
+            WebId        = '1fe70582-a80c-470e-a8c2-8a9cf64e15a8'
+            ListId       = 'A0049D75-96EA-48E2-BBA4-443D5C7D5175'
+            FolderId     = '9050e1a2-39f3-4063-8afd-c64c5dc43851'
+            WebUrl       = 'https://wstllc.sharepoint.com/sites/admin'
+            FolderUrl    = 'https://wstllc.sharepoint.com/sites/admin/Shared Documents/General'
+        },
+        @{
+            TenantTitle  = 'WST'
+            WebTitle     = 'Admin/Accounting'
+            ChannelTitle = 'General'
+            GroupId      = '501d6636-c197-4307-8b01-556fc891fd89'
+            SiteId       = '9c8e611d-7814-4d2d-9455-7e09b1c3f9d4'
+            WebId        = '1fe70582-a80c-470e-a8c2-8a9cf64e15a8'
+            ListId       = 'A0049D75-96EA-48E2-BBA4-443D5C7D5175'
+            FolderId     = '67cd41da-c78d-4f84-8cc6-0a8d260412aa'
+            WebUrl       = 'https://wstllc.sharepoint.com/sites/adminaccounting'
+            FolderUrl    = 'https://wstllc.sharepoint.com/sites/adminaccounting/Shared Documents/General'
+        }
+    )}
 }
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
-    Body = $body
+    Body = $body | ConvertTo-Json
 })
